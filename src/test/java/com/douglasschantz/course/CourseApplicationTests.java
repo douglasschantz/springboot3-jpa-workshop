05e61d75a5b8e387b7503a226f5d18f049eb6a97
douglasschantz <schantzdouglas@gmail.com>
2023-07-31 19:23:52 -0300

    Project created

---
+++ src/test/java/com/douglasschantz/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasschantz.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
