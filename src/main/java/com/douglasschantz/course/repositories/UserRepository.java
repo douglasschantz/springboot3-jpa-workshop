9110843c231f9a3c4afa22006b3b967719210157
douglasschantz <schantzdouglas@gmail.com>
2023-08-01 13:56:26 -0300

    Service layer, component registration

--- src/main/java/com/douglasschantz/course/repositories/UserRepository.java
+++ src/main/java/com/douglasschantz/course/repositories/UserRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.douglasschantz.course.entities.User;
 
-
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
