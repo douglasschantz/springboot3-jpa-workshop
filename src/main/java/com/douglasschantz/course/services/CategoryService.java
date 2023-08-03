7210f229d381525c84af6810b208fd3145015f6f
douglasschantz <schantzdouglas@gmail.com>
2023-08-03 14:37:51 -0300

    Category

---
+++ src/main/java/com/douglasschantz/course/services/CategoryService.java
@@ -0,0 +1,28 @@
+package com.douglasschantz.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.douglasschantz.course.entities.Category;
+import com.douglasschantz.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;	
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
