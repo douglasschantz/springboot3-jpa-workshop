c9839d06cacd41757711ba247b482d06881c5ec7
douglasschantz <schantzdouglas@gmail.com>
2023-08-04 14:08:51 -0300

    OrderItem, many-to-many association with extra attributes - parte 2

--- src/main/java/com/douglasschantz/course/config/TestConfig.java
+++ src/main/java/com/douglasschantz/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.douglasschantz.course.entities.Category;
 import com.douglasschantz.course.entities.Order;
+import com.douglasschantz.course.entities.OrderItem;
 import com.douglasschantz.course.entities.Product;
 import com.douglasschantz.course.entities.User;
 import com.douglasschantz.course.entities.enums.OrderStatus;
 import com.douglasschantz.course.repositories.CategoryRepository;
+import com.douglasschantz.course.repositories.OrderItemRepository;
 import com.douglasschantz.course.repositories.OrderRepository;
 import com.douglasschantz.course.repositories.ProductRepository;
 import com.douglasschantz.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -66,6 +71,12 @@ public class TestConfig implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
 	}
 	
