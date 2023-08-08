67428db956ed5c67b5a6d310ba14bf42ab829e83
douglasschantz <schantzdouglas@gmail.com>
2023-08-08 17:39:56 -0300

    Payment, one-to-one association

--- src/main/java/com/douglasschantz/course/config/TestConfig.java
+++ src/main/java/com/douglasschantz/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.douglasschantz.course.entities.Category;
 import com.douglasschantz.course.entities.Order;
 import com.douglasschantz.course.entities.OrderItem;
+import com.douglasschantz.course.entities.Payment;
 import com.douglasschantz.course.entities.Product;
 import com.douglasschantz.course.entities.User;
 import com.douglasschantz.course.entities.enums.OrderStatus;
@@ -60,8 +61,7 @@ public class TestConfig implements CommandLineRunner{
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));		
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
@@ -78,6 +78,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 	
 	
