c1724e09218bfd42168d7e72435a0f3ed27755f8
douglasschantz <schantzdouglas@gmail.com>
2023-08-09 14:22:07 -0300

    Exception handling - findById

--- src/main/java/com/douglasschantz/course/resources/UserResource.java
+++ src/main/java/com/douglasschantz/course/resources/UserResource.java
@@ -55,7 +55,5 @@ public class UserResource {
 	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
 		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
-	}
-	
-	
+	}	
 }
